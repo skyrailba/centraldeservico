9db67b7bc50b26ece7b826f24151b293e8317852
skyrailba <100210307+skyrailba@users.noreply.github.com>
2022-03-02 10:12:09 -0300

    retirando versão 01

--- 001/Login_v1/js/crypt.js
+++
@@ -1,6 +0,0 @@
-(function myFunction() {
-  var uri = "https://docs.google.com/presentation/d/e/2PACX-1vSRHS0SD9iES3kAjEKlt5wLYpgteZrUa2A66ceagJjCsHCJdAJayyXSJGS57ZPBDg/embed?start=false&loop=true&delayms=60000";
-  var res = encodeURI(uri);
-  document.getElementById("imageoption").src = res;
-}
-)
